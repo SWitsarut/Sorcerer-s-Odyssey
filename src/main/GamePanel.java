e193697e692bfdb9ab04ad4f4f6258e3e9912c3e
SueaWS <witsarut1133@outlook.com>
2023-09-12 13:54:25 +0700

    add animation

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -16,7 +16,9 @@ public class GamePanel extends JPanel {
 
     MouseInput mouseInput;
     private double deltaX = 100, deltaY = 100;
-    private BufferedImage img, subImage;
+    private BufferedImage img;
+    private BufferedImage[] idleAni, walkAni;
+    private int aniTick, aniIndex, aniFramePersecond = 15;
 
     private int tileSize = 32;
 
@@ -40,6 +42,24 @@ public class GamePanel extends JPanel {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        loadIdleAni();
+    }
+
+    private void loadIdleAni() {
+        idleAni = new BufferedImage[10];
+        walkAni = new BufferedImage[10];
+        for (int i = 0; i < idleAni.length; i++) {
+            idleAni[i] = img.getSubimage(i * tileSize, tileSize * 0, tileSize, tileSize);
+            walkAni[i] = img.getSubimage(i * tileSize, tileSize * 4, tileSize, tileSize);
+        }
+    }
+
+    private void updateAnimationTick() {
+        aniTick++;
+        if (aniTick >= aniFramePersecond) {
+            aniTick = 0;
+            aniIndex = (aniIndex + 1) % 10;
+        }
     }
 
     private void setPanelSize() {
@@ -49,18 +69,12 @@ public class GamePanel extends JPanel {
         setMaximumSize(size);
     }
 
-    private int animateX = 0;
-    private int animateY = 0;
-
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        subImage = getNextAnimate(img);
-        g.drawImage(subImage, 0, 0, 64, 64, null);
+
+        updateAnimationTick();
+        g.drawImage(idleAni[aniIndex], 0, 0, 64, 64, null);
+        g.drawImage(walkAni[aniIndex], tileSize, 0, 64, 64, null);
     }
 
-    private BufferedImage getNextAnimate(BufferedImage img) {
-        BufferedImage sub = img.getSubimage(animateX, animateY, tileSize, tileSize);
-        animateX = (animateX + tileSize) % (tileSize * 10);
-        return sub;
-    }
 }
