26b876ae49a9442adee837d4c7527386ced0c575
SueaWS <witsarut1133@outlook.com>
2023-09-12 22:02:18 +0700

    clean unnecessary file

--- tiled/tile set/mage city.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="mage city" tilewidth="16" tileheight="16" tilecount="360" columns="8">
- <image source="../magecity.png" trans="ff00ff" width="128" height="725"/>
-</tileset>
